38233f856d474cfbb6cd4ba6033771f93304fd3d
Rafael Moreira <92860308+raffnb@users.noreply.github.com>
2023-11-20 09:15:23 -0300

    Adicionando primeira funcionalidade para add todo

--- todolistMB/src/components/TodoForm.jsx
+++ todolistMB/src/components/TodoForm.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import {useState} from 'react'
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) =>{
+    //previnir evento
+    // tratando valores sem escrita
+    e.preventDefault();
+    if(!value || !category) return;
+
+    //adcionar todo
+    // limpar os campo
+    setValue("");
+    setCategory("");
+
+    console.log(value,category);
+
+
+  };
+
   return (
     <div className='todo-form'>
         <h2>Criar Tarefa</h2>
-        <form >
-            <input type="text" placeholder='Digite um titulo' />
-            <select>
+        <form  onSubmit={handleSubmit}>
+            <input value={value} type="text" placeholder='Digite um titulo'
+            onChange={(e) => setValue(e.target.value)}
+            
+            />
+            <select value={category} onChange={(e) => setCategory(e.target.value)} > 
                 <option value=""> Selecione uma Categoria</option>
                 <option value="Trabalho"> Trabalho</option>
                 <option value="Pessoal"> Pessoal</option>
