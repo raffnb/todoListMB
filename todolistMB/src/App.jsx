b01c75f413277304994c41c8392c091c845fec28
Rafael Moreira <92860308+raffnb@users.noreply.github.com>
2023-11-24 21:44:50 -0300

    Adicionando a Funcionalidade de Deletar e completar uma tarefa da TodoList

--- todolistMB/src/App.jsx
+++ todolistMB/src/App.jsx
@@ -41,13 +41,29 @@ const addTodo = (text,category)=>{
 
 };
 
+const removeTodo = (id) =>{
+  const newTodos = [...todos];
+  const filteredTodos = newTodos.filter((todo) =>
+  todo.id !== id? todo : null
+  );
+
+  setTodos(filteredTodos);
+};
+
+
+const completeTodo = (id) =>{
+  const newTodos = [...todos]
+  newTodos.map((todo)=> todo.id === id ? todo.isCompleted =!todo.isCompleted : todo);
+  setTodos(newTodos);
+};
+
   return (
     <div>
       <div className="app">
         <h1>Lista de Tarefas</h1>
         <div className="todo-list">
             {todos.map((todo)=>(
-              <Todo key={todo.id} todo ={todo}/>
+              <Todo key={todo.id} todo ={todo} removeTodo={removeTodo} completeTodo= {completeTodo} />
           ))}
         </div>
       <TodoForm addTodo={addTodo} />
